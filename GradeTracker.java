933266d8e382782f2c9f3f0eec266572cbb8e130
Juan Figueroa <juanseb023@gmail.com>
2026-07-20 19:46:30 -0500

    Initial skeleton of the Student and GradeTracker classes.

---
+++ GradeTracker.java
@@ -0,0 +1,30 @@
+// main class that runs the program
+// it would show a menu and let the user manage a list of students
+
+public class GradeTracker {
+
+    // we need would need an ArrayList to hold all our Student objects
+    // we also need a Scanner to read input from the user
+
+    public static void main(String[] args) {
+        // print a welcome message when the program starts
+        // then we could run a while loop that keeps showing the menu until the user exits
+        // inside the loop we could use if/else to call the right method based on what the user picks
+    }
+
+    // Possible work breakdown
+    // Juan: create method that prints the menu options for the user to choose from
+
+    // Juan: create method that asks the user for a name, id, and grades, then creates a Student and adds it to the list
+
+    // Juan: create the method that prints out every student currently in the list
+
+    // Nick: create the method that asks for a name and searches the list for any student that matches
+
+    // Nick: create the method that asks for an id and removes that student from the list
+
+    // Myat: create the method that goes through all students and prints the overall class average
+
+    // Myat: create the method that finds the student with the highest average and prints their info
+
+}
---
+++ Student.java
@@ -0,0 +1,22 @@
+// CSC156 - Java Computer Science 1
+// Members: Juan Figueroa, Myat Min Htoo, Nicholas Korsak
+
+// This class represents a single student
+// for now each student could have fields like name, id, and a list of their grades
+
+public class Student {
+
+    // we could have variables here to store the student's name, id, and grades and any other variables we want to add
+    // grades could be stored in an array of doubles
+
+    // the constructor would go here, it could the variables as parameters
+
+    // we would also need getters so other classes can access the fields
+
+    // possible method that could use a for loop to add up all the grades and return the average
+
+    // possible method that could use if/else to check the average and return the right letter grade (A, B, C, D, or F)
+
+    // possible method to return something like a formatted string showing the student's name, id, average, and letter grade
+
+}
